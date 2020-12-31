e2b1b3e8a689bd23a19394f16bb95bd66b13151a
LxGrey <lx_grey@icloud.com>
2020-12-31 10:11:26 -0800

    Initial Commit

---
+++ SwiftUI Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI Weather
+//
+//  Created by Shelton Scharhag on 12/31/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
